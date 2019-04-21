2129f39d73ba9025412251c79df60a81ff508788
Amer Kumar <amer.tanwani@gmail.com>
2019-04-21 11:24:19 +0500

    Added Location and Region and WayFinding API

--- app/src/main/java/app/com/thecentaurusmall/map/IndoorMapFragment.java
+++ app/src/main/java/app/com/thecentaurusmall/map/IndoorMapFragment.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
@@ -27,17 +28,39 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.indooratlas.android.sdk.IALocation;
+import com.indooratlas.android.sdk.IALocationListener;
 import com.indooratlas.android.sdk.IALocationManager;
 import com.indooratlas.android.sdk.IALocationRequest;
+import com.indooratlas.android.sdk.IAOrientationListener;
 import com.indooratlas.android.sdk.IAOrientationRequest;
+import com.indooratlas.android.sdk.IARegion;
+import com.indooratlas.android.sdk.IARoute;
+import com.indooratlas.android.sdk.IAWayfindingListener;
+import com.indooratlas.android.sdk.IAWayfindingRequest;
+import com.indooratlas.android.sdk.resources.IAFloorPlan;
+import com.indooratlas.android.sdk.resources.IALatLng;
+import com.indooratlas.android.sdk.resources.IALocationListenerSupport;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.RequestCreator;
+import com.squareup.picasso.Target;
+
+import java.util.Deque;
+import java.util.List;
 
 import app.com.thecentaurusmall.MainActivity;
 import app.com.thecentaurusmall.R;
@@ -47,6 +70,11 @@ public class IndoorMapFragment extends Fragment implements
         FloorSelectionDialog.FloorSelectDialogListener
 {
 
+    /* used to decide when bitmap should be downscaled */
+    private static final int MAX_DIMENSION = 2048;
+
+
+    private static final String TAG = IndoorMapFragment.class.getSimpleName();
     private static final int PERMISSIONS_REQUEST_LOCATION = 1;
     private IndoorMapViewModel mViewModel;
     private GoogleMap mMap;
@@ -56,6 +84,14 @@ public class IndoorMapFragment extends Fragment implements
     private IALocationManager mIALocationManager;
     private boolean fabFlag;
     private MaterialButton floorButton;
+    private boolean mCameraPositionNeedsUpdating;
+    private GroundOverlay mGroundOverlay;
+    private IARegion mOverlayFloorPlan;
+    private IAWayfindingRequest mWayfindingDestination;
+    private IARoute mCurrentRoute;
+    private Target mLoadTarget;
+    private View rootView;
+    private List<Polyline> mPolylines;
 
     public static IndoorMapFragment newInstance() {
         return new IndoorMapFragment();
@@ -80,7 +116,7 @@ public class IndoorMapFragment extends Fragment implements
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.indoor_map_fragment, container, false);
+        rootView = inflater.inflate(R.layout.indoor_map_fragment, container, false);
         mapView = rootView.findViewById(R.id.map);
 
         mapView.onCreate(savedInstanceState);
@@ -138,6 +174,7 @@ public class IndoorMapFragment extends Fragment implements
                     Drawable newDrawable = myFabSrc.getConstantState().newDrawable();
                     newDrawable.mutate().setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_ATOP);
                     floatingActionButton.setImageDrawable(newDrawable);
+                    stopLocationUpdates();
                     fabFlag = false;
                 }
                 // if false
@@ -149,6 +186,7 @@ public class IndoorMapFragment extends Fragment implements
                     Drawable newDrawable = myFabSrc.getConstantState().newDrawable();
                     newDrawable.mutate().setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
                     floatingActionButton.setImageDrawable(newDrawable);
+                    startLocationUpdates();
                     fabFlag = true;
                 }
             }
@@ -210,19 +248,11 @@ public class IndoorMapFragment extends Fragment implements
         mapView.getMapAsync(this);
 
         if (fabFlag) {
-            // start receiving location updates & monitor region changes
-            mIALocationManager.requestLocationUpdates(IALocationRequest.create(), mListener);
-            mIALocationManager.registerRegionListener(mRegionListener);
-            mIALocationManager.registerOrientationListener(
-                    // update if heading changes by 1 degrees or more
-                    new IAOrientationRequest(1, 0),
-                    mOrientationListener);
-
-            if (mWayfindingDestination != null) {
-                mIALocationManager.requestWayfindingUpdates(mWayfindingDestination, mWayfindingListener);
-            }
+            startLocationUpdates();
         }
     }
+
+
 
 
     @Override
@@ -303,6 +333,34 @@ public class IndoorMapFragment extends Fragment implements
 
 //    Location Methods
 
+
+    private IAWayfindingListener mWayfindingListener = new IAWayfindingListener() {
+        @Override
+        public void onWayfindingUpdate(IARoute route) {
+            mCurrentRoute = route;
+            if (hasArrivedToDestination(route)) {
+                // stop wayfinding
+                showInfo("You're there!");
+                mCurrentRoute = null;
+                mWayfindingDestination = null;
+                mIALocationManager.removeWayfindingUpdates();
+            }
+            updateRouteVisualization();
+        }
+    };
+
+    private IAOrientationListener mOrientationListener = new IAOrientationListener() {
+        @Override
+        public void onHeadingChanged(long timestamp, double heading) {
+            updateHeading(heading);
+        }
+
+        @Override
+        public void onOrientationChange(long timestamp, double[] quaternion) {
+            // we do not need full device orientation in this example, just the heading
+        }
+    };
+
     private void showLocationCircle(LatLng center, double accuracyRadius) {
         if (mCircle == null) {
             // location can received before map is initialized, ignoring those updates
@@ -336,6 +394,72 @@ public class IndoorMapFragment extends Fragment implements
         }
     }
 
+
+
+    private int mFloor;
+
+    /**
+     * Listener that handles location change events.
+     */
+    private IALocationListener mListener = new IALocationListenerSupport() {
+
+
+        /**
+         * Location changed, move marker and camera position.
+         */
+        @Override
+        public void onLocationChanged(IALocation location) {
+
+            Log.d(TAG, "new location received with coordinates: " + location.getLatitude()
+                    + "," + location.getLongitude());
+
+            if (mMap == null) {
+                // location received before map is initialized, ignoring update here
+                return;
+            }
+
+            final LatLng center = new LatLng(location.getLatitude(), location.getLongitude());
+
+            final int newFloor = location.getFloorLevel();
+            if (mFloor != newFloor) {
+                updateRouteVisualization();
+            }
+            mFloor = newFloor;
+
+            showLocationCircle(center, location.getAccuracy());
+
+            // our camera position needs updating if location has significantly changed
+            if (mCameraPositionNeedsUpdating) {
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(center, 17.5f));
+                mCameraPositionNeedsUpdating = false;
+            }
+        }
+    };
+
+    /**
+     * Listener that changes overlay if needed
+     */
+    private IARegion.Listener mRegionListener = new IARegion.Listener() {
+        @Override
+        public void onEnterRegion(IARegion region) {
+            if (region.getType() == IARegion.TYPE_FLOOR_PLAN) {
+                Log.d(TAG, "enter floor plan " + region.getId());
+                mCameraPositionNeedsUpdating = true; // entering new fp, need to move camera
+                if (mGroundOverlay != null) {
+                    mGroundOverlay.remove();
+                    mGroundOverlay = null;
+                }
+                mOverlayFloorPlan = region; // overlay will be this (unless error in loading)
+                fetchFloorPlanBitmap(region.getFloorPlan());
+            }
+        }
+
+        @Override
+        public void onExitRegion(IARegion region) {
+        }
+
+    };
+
     public void onDialogFloorClick(int which) {
         switch (which) {
             case 0:
@@ -359,4 +483,208 @@ public class IndoorMapFragment extends Fragment implements
 
         }
     }
+
+    private void startLocationUpdates() {
+        // start receiving location updates & monitor region changes
+        mIALocationManager.requestLocationUpdates(IALocationRequest.create(), mListener);
+        mIALocationManager.registerRegionListener(mRegionListener);
+        mIALocationManager.registerOrientationListener(
+                // update if heading changes by 1 degrees or more
+                new IAOrientationRequest(1, 0),
+                mOrientationListener);
+
+        if (mWayfindingDestination != null) {
+            mIALocationManager.requestWayfindingUpdates(mWayfindingDestination, mWayfindingListener);
+        }
+    }
+
+
+    private void stopLocationUpdates() {
+        // unregister location & region changes
+        mIALocationManager.removeLocationUpdates(mListener);
+        mIALocationManager.unregisterRegionListener(mRegionListener);
+        mIALocationManager.unregisterOrientationListener(mOrientationListener);
+
+        if (mWayfindingDestination != null) {
+            mIALocationManager.removeWayfindingUpdates();
+        }
+    }
+
+
+    /**
+     * Sets bitmap of floor plan as ground overlay on Google Maps
+     */
+    private void setupGroundOverlay(IAFloorPlan floorPlan, Bitmap bitmap) {
+
+        if (mGroundOverlay != null) {
+            mGroundOverlay.remove();
+        }
+
+        if (mMap != null) {
+            BitmapDescriptor bitmapDescriptor = BitmapDescriptorFactory.fromBitmap(bitmap);
+            IALatLng iaLatLng = floorPlan.getCenter();
+            LatLng center = new LatLng(iaLatLng.latitude, iaLatLng.longitude);
+            GroundOverlayOptions fpOverlay = new GroundOverlayOptions()
+                    .image(bitmapDescriptor)
+                    .zIndex(0.0f)
+                    .position(center, floorPlan.getWidthMeters(), floorPlan.getHeightMeters())
+                    .bearing(floorPlan.getBearing());
+
+            mGroundOverlay = mMap.addGroundOverlay(fpOverlay);
+        }
+    }
+
+    /**
+     * Download floor plan using Picasso library.
+     */
+    private void fetchFloorPlanBitmap(final IAFloorPlan floorPlan) {
+
+        if (floorPlan == null) {
+            Log.e(TAG, "null floor plan in fetchFloorPlanBitmap");
+            return;
+        }
+
+        final String url = floorPlan.getUrl();
+        Log.d(TAG, "loading floor plan bitmap from "+url);
+
+        mLoadTarget = new Target() {
+
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                Log.d(TAG, "onBitmap loaded with dimensions: " + bitmap.getWidth() + "x"
+                        + bitmap.getHeight());
+                if (mOverlayFloorPlan != null && floorPlan.getId().equals(mOverlayFloorPlan.getId())) {
+                    Log.d(TAG, "showing overlay");
+                    setupGroundOverlay(floorPlan, bitmap);
+                }
+            }
+
+            @Override
+            public void onBitmapFailed(Exception e, Drawable errorDrawable) {
+                showInfo("Failed to load bitmap");
+                mOverlayFloorPlan = null;
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+                // N/A
+            }
+        };
+
+
+
+        RequestCreator request =
+                Picasso.get().load(url);
+
+        final int bitmapWidth = floorPlan.getBitmapWidth();
+        final int bitmapHeight = floorPlan.getBitmapHeight();
+
+        if (bitmapHeight > MAX_DIMENSION) {
+            request.resize(0, MAX_DIMENSION);
+        } else if (bitmapWidth > MAX_DIMENSION) {
+            request.resize(MAX_DIMENSION, 0);
+        }
+
+        request.into(mLoadTarget);
+    }
+
+    private void showInfo(String text) {
+        final Snackbar snackbar = Snackbar.make(rootView.findViewById(android.R.id.content), text,
+                Snackbar.LENGTH_INDEFINITE);
+        snackbar.setAction("Close", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                snackbar.dismiss();
+            }
+        });
+        snackbar.show();
+    }
+
+//    @Override
+//    public void onMapClick(LatLng point) {
+//        if (mMap != null) {
+//
+//            mWayfindingDestination = new IAWayfindingRequest.Builder()
+//                    .withFloor(mFloor)
+//                    .withLatitude(point.latitude)
+//                    .withLongitude(point.longitude)
+//                    .build();
+//
+//            mIALocationManager.requestWayfindingUpdates(mWayfindingDestination, mWayfindingListener);
+//
+//            if (mDestinationMarker == null) {
+//                mDestinationMarker = mMap.addMarker(new MarkerOptions()
+//                        .position(point)
+//                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+//            } else {
+//                mDestinationMarker.setPosition(point);
+//            }
+//            Log.d(TAG, "Set destination: (" + mWayfindingDestination.getLatitude() + ", " +
+//                    mWayfindingDestination.getLongitude() + "), floor=" +
+//                    mWayfindingDestination.getFloor());
+//        }
+//    }
+
+    private boolean hasArrivedToDestination(IARoute route) {
+        // empty routes are only returned when there is a problem, for example,
+        // missing or disconnected routing graph
+        if (route.getLegs().size() == 0) {
+            return false;
+        }
+
+        final double FINISH_THRESHOLD_METERS = 8.0;
+        double routeLength = 0;
+        for (IARoute.Leg leg : route.getLegs()) routeLength += leg.getLength();
+        return routeLength < FINISH_THRESHOLD_METERS;
+    }
+
+    /**
+     * Clear the visualizations for the wayfinding paths
+     */
+    private void clearRouteVisualization() {
+        for (Polyline pl : mPolylines) {
+            pl.remove();
+        }
+        mPolylines.clear();
+    }
+
+    /**
+     * Visualize the IndoorAtlas Wayfinding route on top of the Google Maps.
+     */
+    private void updateRouteVisualization() {
+
+        clearRouteVisualization();
+
+        if (mCurrentRoute == null) {
+            return;
+        }
+
+        for (IARoute.Leg leg : mCurrentRoute.getLegs()) {
+
+            if (leg.getEdgeIndex() == null) {
+                // Legs without an edge index are, in practice, the last and first legs of the
+                // route. They connect the destination or current location to the routing graph.
+                // All other legs travel along the edges of the routing graph.
+
+                // Omitting these "artificial edges" in visualization can improve the aesthetics
+                // of the route. Alternatively, they could be visualized with dashed lines.
+                continue;
+            }
+
+            PolylineOptions opt = new PolylineOptions();
+            opt.add(new LatLng(leg.getBegin().getLatitude(), leg.getBegin().getLongitude()));
+            opt.add(new LatLng(leg.getEnd().getLatitude(), leg.getEnd().getLongitude()));
+
+            // Here wayfinding path in different floor than current location is visualized in
+            // a semi-transparent color
+            if (leg.getBegin().getFloor() == mFloor && leg.getEnd().getFloor() == mFloor) {
+                opt.color(0xFF0000FF);
+            } else {
+                opt.color(0x300000FF);
+            }
+
+            mPolylines.add(mMap.addPolyline(opt));
+        }
+    }
+
 }
